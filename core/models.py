0749f64275a6ba6d7442bf7df776f782bbeb10be
Slyr <jezizz@mail.ru>
2024-05-01 23:04:10 +0500

    probe

---
+++ core/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Car(models.Model):
+    name = models.CharField('Название', max_length=100)
+
+    class Meta:
+        verbose_name = 'Машина'
+        verbose_name_plural = 'Машины'
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField('Имя', max_length=255)
+    age = models.IntegerField()
+    grade = models.FloatField()
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+
+
+    def __str__(self) -> str:
+        return self.name
---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from core import views
+
+
+urlpatterns = [
+    path('time/', views.current_datetime),
+]
