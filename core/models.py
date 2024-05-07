09c58341158e159c7f453280e66b633486762637
Slyr <jezizz@mail.ru>
2024-05-07 22:20:34 +0500

    Добавил пару строк в модели

--- core/models.py
+++ core/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 
 class Car(models.Model):
-    name = models.CharField('Название', max_length=100)
+
+    TYPE_CHOICE = (
+        ('hatchback', 'хэтчбек'),
+        ('saloon', 'седан'),
+        ('estate', 'универсал'),
+        ('convertible', 'кабриолет')
+    )
+
+    car_name = models.CharField('Название', max_length=100)
+    car_body = models.CharField('Кузов', max_length=100, choices=TYPE_CHOICE, default='saloon')
+    information = models.TextField('Информация', null=True, blank=True)
+
 
     class Meta:
         verbose_name = 'Машина'
@@ -10,13 +21,17 @@ class Car(models.Model):
 
 
     def __str__(self) -> str:
-        return self.name
+        return self.car_name
 
 
 class Student(models.Model):
-    name = models.CharField('Имя', max_length=255)
-    age = models.IntegerField('Возраст')
+    first_name = models.CharField('Фамилия', max_length=20, default='')
+    last_name = models.CharField('Имя', max_length=10, default='')
+    age = models.PositiveIntegerField('Возраст')
+
     grade = models.FloatField('Оценка')
+    expelled = models.BooleanField('Исключен', default=False)
+
 
     class Meta:
         verbose_name = 'Студент'
@@ -24,4 +39,4 @@ class Student(models.Model):
 
 
     def __str__(self) -> str:
-        return self.name
+        return f'{self.first_name} {self.last_name}'
