dfa22e5903511840b14b60e98a5727520771f58d
Slyr <jezizz@mail.ru>
2024-05-08 23:37:43 +0500

    + фото

--- core/models.py
+++ core/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Car(models.Model):
-
     TYPE_CHOICE = (
         ('hatchback', 'хэтчбек'),
         ('saloon', 'седан'),
@@ -13,12 +12,11 @@ class Car(models.Model):
     car_name = models.CharField('Название', max_length=100)
     car_body = models.CharField('Кузов', max_length=100, choices=TYPE_CHOICE, default='saloon')
     information = models.TextField('Информация', null=True, blank=True)
-
+    car_img = models.ImageField('Фото', upload_to='pictures', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Машина'
         verbose_name_plural = 'Машины'
-
 
     def __str__(self) -> str:
         return self.car_name
@@ -32,11 +30,9 @@ class Student(models.Model):
     grade = models.FloatField('Оценка')
     expelled = models.BooleanField('Исключен', default=False)
 
-
     class Meta:
         verbose_name = 'Студент'
         verbose_name_plural = 'Студенты'
-
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
--- core/views.py
+++ core/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 
+from core import models
+
 
 def current_datetime(request):
     now = datetime.datetime.today().strftime('%d.%m.%Y %H:%M')
     html = f"Сейчас {now}."
     return HttpResponse(html)
+
+def index(request):
+    car_info = models.Car.objects.all()
+    student_info = models.Student.objects.all()
+    context = {
+        'title': 'Списки',
+        'cars': 'Список машин',
+        'students': 'Список студентов',
+        'car_info': car_info,
+        'student_info': student_info,
+    }
+    return render(request, 'core/index.html', context=context)
 
